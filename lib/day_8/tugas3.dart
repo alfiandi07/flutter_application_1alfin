b5ed2415b9b0075d31752c35221604207a62c395
alfiandi07 <alfiandisha@gmail.com>
2026-07-27 11:15:20 +0700

    tugas 3

--- lib/day_8/tugas3.dart
+++ lib/day_8/tugas3.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-class TugasWidget3 extends StatelessWidget {
-  const TugasWidget3({super.key});
+class Tugas3 extends StatelessWidget {
+  const Tugas3({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Registrasi & Katalog",
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          "Registrasi & Login",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
-        backgroundColor: Colors.teal,
+        backgroundColor: Color(0xFF2196F3),
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
@@ -24,30 +24,86 @@ class TugasWidget3 extends StatelessWidget {
               padding: EdgeInsets.all(8.0),
               child: Container(
                 decoration: BoxDecoration(
-                  color: const Color.fromARGB(230, 193, 193, 196),
+                  color: Color.fromARGB(244, 231, 228, 228),
                   borderRadius: BorderRadius.circular(15),
                 ),
                 margin: EdgeInsets.all(9),
                 height: 400,
                 width: 500,
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsetsGeometry.all(9.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Text(
                         "Form Registrasi",
                         style: TextStyle(
-                          fontSize: 20,
+                          fontSize: 18,
                           fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(255, 6, 7, 7),
                         ),
                       ),
+                      SizedBox(height: 20),
+
+                      Container(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Nama Pengguna",
+                            filled: true,
+                            fillColor: Colors.white,
+                            prefixIcon: Icon(Icons.person),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+
                       TextField(
                         decoration: InputDecoration(
-                          hintText: "Masukan Email Anda",
+                          hintText: "Email Pengguna",
+                          filled: true,
+                          fillColor: Colors.white,
+                          prefixIcon: Icon(Icons.email),
                           border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(50),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: "Telepon Pengguna",
+                          filled: true,
+                          fillColor: const Color.fromARGB(255, 255, 255, 255),
+                          prefixIcon: Icon(Icons.phone),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: "Masukan Password Anda",
+                          filled: true,
+                          fillColor: Colors.white,
+                          prefixIcon: Icon(Icons.lock),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: "Konfirmasi Password Anda",
+                          filled: true,
+                          fillColor: Colors.white,
+                          prefixIcon: Icon(Icons.visibility),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
                           ),
                         ),
                       ),
@@ -55,6 +111,181 @@ class TugasWidget3 extends StatelessWidget {
                   ),
                 ),
               ),
+            ),
+            Text(
+              "Destinasti Wisata Jakarta",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            GridView.count(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              crossAxisCount: 3, // Menampilkan 2 kotak per baris
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              children: <Widget>[
+                Stack(
+                  alignment: AlignmentGeometry.bottomCenter,
+                  children: [
+                    Container(
+                      height: 300,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/kota tua.jpg"),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                      width: 100,
+                      color: const Color.fromARGB(255, 135, 134, 134),
+                      child: Text(
+                        "Kota Tua",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentGeometry.bottomCenter,
+                  children: [
+                    Container(
+                      height: 300,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/monas.jpg"),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                      width: 100,
+                      color: const Color.fromARGB(121, 13, 13, 13),
+                      child: Text(
+                        "Monas",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentGeometry.bottomCenter,
+                  children: [
+                    Container(
+                      height: 300,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/dufan.jpg"),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                      width: 100,
+                      color: const Color.fromARGB(255, 104, 103, 103),
+                      child: Text(
+                        "Dufan",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentGeometry.bottomCenter,
+                  children: [
+                    Container(
+                      height: 300,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/planetariumJakarta.jpg"),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                      width: 100,
+                      color: const Color.fromARGB(255, 127, 126, 126),
+                      child: Text(
+                        "Planetarium",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentGeometry.bottomCenter,
+                  children: [
+                    Container(
+                      height: 300,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/TMII.jpg"),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                      width: 100,
+                      color: const Color.fromARGB(255, 130, 129, 129),
+                      child: Text(
+                        "TMII",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentGeometry.bottomCenter,
+                  children: [
+                    Container(
+                      height: 300,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/museum BI.jpg"),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                      width: 100,
+                      color: const Color.fromARGB(221, 126, 126, 126),
+                      child: Text(
+                        "Museum BI",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
