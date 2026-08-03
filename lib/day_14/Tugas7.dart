49249c459b3fd814d92209631ec196faac544210
alfiandi07 <alfiandisha@gmail.com>
2026-08-03 14:52:28 +0700

    Tugas 7

---
+++ lib/day_14/Tugas7.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Tugas7flutter extends StatefulWidget {
+  const Tugas7flutter({super.key});
+
+  @override
+  State<Tugas7flutter> createState() => _Tugas7flutterState();
+}
+
+class _Tugas7flutterState extends State<Tugas7flutter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [Text("persyaratan")]));
+  }
+}
