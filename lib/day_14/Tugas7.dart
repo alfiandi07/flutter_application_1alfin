db7f7b9b60aeb6e95f63b8edfbfd1b1dad8574d0
alfiandi07 <alfiandisha@gmail.com>
2026-08-04 13:34:18 +0700

    Tugas 7 dan 8

--- lib/day_14/Tugas7.dart
+++ lib/day_14/Tugas7.dart
@@ -1,15 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1alfin/day_14/Kategori.dart';
+import 'package:flutter_application_1alfin/day_14/checkbox.dart';
+import 'package:flutter_application_1alfin/day_14/date.dart';
+import 'package:flutter_application_1alfin/day_14/switch.dart';
+import 'package:flutter_application_1alfin/day_14/time.dart';
 
-class Tugas7flutter extends StatefulWidget {
-  const Tugas7flutter({super.key});
+class Tugas7Day14 extends StatefulWidget {
+  const Tugas7Day14({super.key});
 
   @override
-  State<Tugas7flutter> createState() => _Tugas7flutterState();
+  State<Tugas7Day14> createState() => _Tugas7Day14State();
 }
 
-class _Tugas7flutterState extends State<Tugas7flutter> {
+class _Tugas7Day14State extends State<Tugas7Day14> {
+  int _selectedBottom = 0;
+
+  void changeBottom(int index) {
+    _selectedBottom = index;
+    print("Ini adalah value dari $_selectedBottom");
+    setState(() {});
+  }
+
+  final List<Widget> _widgetOption = [
+    CheckBoxTugas(),
+    TugasSwitchAlfin(),
+    TugasKategoriAlfin(),
+    TugasDateAlfin(),
+    TugasTimeAlfin(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Text("persyaratan")]));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Input Interaktif",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
+        ),
+        backgroundColor: Colors.indigo,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.check_box_outline_blank_outlined),
+              title: Text("Syarat dan Ketentuan"),
+              onTap: () {
+                changeBottom(0);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.mode_outlined),
+              title: Text("Mode Tampilan"),
+              onTap: () {
+                changeBottom(1);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.category),
+              title: Text("Kategori"),
+              onTap: () {
+                changeBottom(2);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.calendar_month),
+              title: Text("Pilih Tanggal"),
+              onTap: () {
+                changeBottom(3);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.timer),
+              title: Text("Atur Pengingat"),
+              onTap: () {
+                changeBottom(4);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: _widgetOption.elementAt(_selectedBottom),
+    );
   }
 }
---
+++ lib/day_14/Tugas8.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1alfin/day_14/Tugas7.dart';
+import 'package:flutter_application_1alfin/day_14/tentangapk.dart';
+
+class Tugas8Day14 extends StatefulWidget {
+  const Tugas8Day14({super.key});
+
+  @override
+  State<Tugas8Day14> createState() => _Tugas8Day14State();
+}
+
+class _Tugas8Day14State extends State<Tugas8Day14> {
+  int _selectedBottom = 0;
+
+  void changeBottom(int index) {
+    _selectedBottom = index;
+    print("ini adalah value dari $_selectedBottom");
+    setState(() {});
+  }
+
+  final List<Widget> _widgetOption = [Tugas7Day14(), Tugas4Flutter()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          changeBottom(value);
+        },
+        currentIndex: _selectedBottom,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.app_blocking),
+            label: "Tentang Aplikasi",
+          ),
+        ],
+      ),
+      body: _widgetOption.elementAt(_selectedBottom),
+    );
+  }
+}
